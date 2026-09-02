8f7db7b691fe7b4d7da6c8b86cdca40d8ecb47f9
JESUS ALEXANDER LAUREANO MENJIVAR <jesuslaureano085@gmail.com>
2026-09-02 09:55:44 -0600

    Parcial de progra

---
+++ Estadistico/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Sistema_estadistico__calcule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(29, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(796, 32);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "1.Ingrese el Dataset (edades,minutos o horas,separados por comas):";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(29, 43);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(722, 117);
+            this.txt1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(78, 166);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(620, 59);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Procesar dataset";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(12, 242);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(303, 32);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "2. Resultado Estadisticos:";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(12, 276);
+            this.txt2.Multiline = true;
+            this.txt2.Name = "txt2";
+            this.txt2.ReadOnly = true;
+            this.txt2.Size = new System.Drawing.Size(739, 115);
+            this.txt2.TabIndex = 4;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(12, 405);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(408, 32);
+            this.lbl3.TabIndex = 5;
+            this.lbl3.Text = "3.Explicacion teorica (diferencias):";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(78, 455);
+            this.txt3.Multiline = true;
+            this.txt3.Name = "txt3";
+            this.txt3.ReadOnly = true;
+            this.txt3.Size = new System.Drawing.Size(593, 62);
+            this.txt3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 529);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txt3;
+    }
+}
